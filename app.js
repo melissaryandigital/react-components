7a7d5e29e4b23185218ac7fdbb63ea13563c6605
Melissa Ryan <melissaryandigital@gmail.com>
2020-11-30 12:22:54 -0600

    Dynamically generate grocery list

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // TODO
+
+var groceryItems = ['Eggs', 'Coffee', 'Jalapenos'];
+
+// displays one item as a list item
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+)
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+)
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={groceryItems} />
+  </div>
+)
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
